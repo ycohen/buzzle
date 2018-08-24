ac5f32adfd19b652c9aea8391be757630c0af05a
Yonatan Cohen <yonatanmc@gmail.com>
2018-08-23 21:02:37 -0700

    Added method for drawing the triangles

--- src/main.js
+++ src/main.js
@@ -18,28 +18,34 @@
         ctx.arc(x, y, disk.radius, 0, Math.PI* 2, true);
         ctx.fill();
 
-        ctx.beginPath();
         ctx.fillStyle = 'grey';
-        ctx.moveTo(x, y - disk.radius);
-        ctx.lineTo(x + Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
-        ctx.lineTo(x - Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
-        ctx.lineTo(x, y - disk.radius);
-        ctx.fill();
-        ctx.stroke();
+
+        drawTriangle([
+            {x: x, y: y - disk.radius},
+            {x: x + Math.sqrt(3) * disk.radius / 4, y: y - disk.radius / 4},
+            {x: x - Math.sqrt(3) * disk.radius / 4, y: y - disk.radius / 4}
+        ], ctx);
         
-        ctx.beginPath();
-        ctx.moveTo(x - Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
-        ctx.lineTo(x - Math.sqrt(3) * disk.radius / 2, y + disk.radius / 2);
-        ctx.lineTo(x, y + disk.radius / 2);
-        ctx.lineTo(x - Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
-        ctx.fill();
-        ctx.stroke();
+        drawTriangle([
+            {x: x - Math.sqrt(3) * disk.radius / 4, y: y - disk.radius / 4},
+            {x: x - Math.sqrt(3) * disk.radius / 2, y: y + disk.radius / 2},
+            {x: x, y: y + disk.radius / 2}
+        ], ctx);
         
+        drawTriangle([
+            {x: x + Math.sqrt(3) * disk.radius / 4, y: y - disk.radius / 4},
+            {x: x + Math.sqrt(3) * disk.radius / 2, y: y + disk.radius / 2},
+            {x: x, y: y + disk.radius / 2}
+        ], ctx);
+    }
+
+    function drawTriangle(vertices, ctx) {
         ctx.beginPath();
-        ctx.moveTo(x + Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
-        ctx.lineTo(x + Math.sqrt(3) * disk.radius / 2, y + disk.radius / 2);
-        ctx.lineTo(x, y + disk.radius / 2);
-        ctx.lineTo(x + Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.moveTo(vertices[vertices.length - 1].x, vertices[vertices.length - 1].y);
+
+        vertices.forEach(function(vertex) {
+            ctx.lineTo(vertex.x, vertex.y);
+        });
         ctx.fill();
         ctx.stroke();
     }
