e742d5061c75cbdf1d1ba15add35d3f6cb805af0
Yonatan Cohen <yonatanmc@gmail.com>
2018-08-23 20:49:24 -0700

    Draws triangles in disks

--- src/main.js
+++ src/main.js
@@ -9,11 +9,39 @@
         ctx.beginPath();
         ctx.fillStyle = 'brown';
         if (disk.row % 2 == 0) {
-            ctx.arc(2 * disk.radius * (disk.col + 1), (disk.row * Math.sqrt(3) + 1) * disk.radius, disk.radius, 0, Math.PI* 2, true);
+            var x = 2 * disk.radius * (disk.col + 1);
+            var y = (disk.row * Math.sqrt(3) + 1) * disk.radius;
         } else {
-            ctx.arc(disk.radius * (2 * disk.col + 1), (disk.row * Math.sqrt(3) + 1) * disk.radius, disk.radius, 0, Math.PI* 2, true);
+            var x = disk.radius * (2 * disk.col + 1);
+            var y = (disk.row * Math.sqrt(3) + 1) * disk.radius;
         }
+        ctx.arc(x, y, disk.radius, 0, Math.PI* 2, true);
         ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = 'grey';
+        ctx.moveTo(x, y - disk.radius);
+        ctx.lineTo(x + Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.lineTo(x - Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.lineTo(x, y - disk.radius);
+        ctx.fill();
+        ctx.stroke();
+        
+        ctx.beginPath();
+        ctx.moveTo(x - Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.lineTo(x - Math.sqrt(3) * disk.radius / 2, y + disk.radius / 2);
+        ctx.lineTo(x, y + disk.radius / 2);
+        ctx.lineTo(x - Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.fill();
+        ctx.stroke();
+        
+        ctx.beginPath();
+        ctx.moveTo(x + Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.lineTo(x + Math.sqrt(3) * disk.radius / 2, y + disk.radius / 2);
+        ctx.lineTo(x, y + disk.radius / 2);
+        ctx.lineTo(x + Math.sqrt(3) * disk.radius / 4, y - disk.radius / 4);
+        ctx.fill();
+        ctx.stroke();
     }
     
     board = {
